9efb2bf411fe6126982dfd774b35ba7fed963bd1
Ahmed00 <ahmedismael12@gmail.com>
2020-11-28 20:58:31 +0100

    using material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from "./logo.svg";
 
 import Joke from "./components/Joke";
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3),
+    textAlign:'center'
+  }
+
+}));
 
 function App() {
+  const classes = useStyles();
 
+  const handleReload  =() => {
+    window.location.reload() //TODO: for more complex app use refetch and useQuery apollo or use setState hook
+  }
 
   return (
-    <div align='center'>
-      <Joke />
+    <div className={classes.root}>
+      <Button variant='outlined' onClick={handleReload}>Next random Joke</Button>
+      <Paper elevation={2}>
+        <Joke />
+      </Paper>
+      
     </div>
   );
 }
